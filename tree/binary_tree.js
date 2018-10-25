6aa1347d3087eb7317b70cdcdf63fba60e2b35b0
xu.mingxun <xu.mingxun@puscene.com>
2018-10-25 15:58:26 +0800

    add

--- tree/binary_tree.js
+++ tree/binary_tree.js
@@ -2,8 +2,8 @@ let Node=require('./node.js')
 
 //1red 0black
 
-let BinaryTree=function(root) {
-    this.root=root;
+let BinaryTree=function() {
+    this.root=null;
     this.insert=function(node){
         this.root=this.insertNode(this.root,node);
     }
@@ -60,7 +60,7 @@ let BinaryTree=function(root) {
     }
     this.insertNode=function(root,node){
         if (root == null) {
-            return null;
+            return node;
         }else{
             if(root.data<node.data){
                 if(root.rightChild==null){
@@ -103,18 +103,30 @@ let BinaryTree=function(root) {
     }
 }
 
-let root = new Node(10, 0, null, null);
-let tree = new BinaryTree(root);
+let seed=1;
+var random=function(max){
+    return parseInt(Math.sin(seed++)*max)+max;
+}
 
-tree.insert(new Node(20, 0, null, null));
-tree.insert(new Node(15, 0, null, null));
-tree.insert(new Node(8, 0, null, null));
-tree.insert(new Node(6, 0, null, null));
-tree.insert(new Node(9, 0, null, null));
-tree.insert(new Node(13, 0, null, null));
-tree.insert(new Node(16, 0, null, null));
+// let root = new Node(10, 0, null, null);
+let tree = new BinaryTree();
+
+// tree.insert(new Node(20, 0, null, null));
+// tree.insert(new Node(15, 0, null, null));
+// tree.insert(new Node(8, 0, null, null));
+// tree.insert(new Node(6, 0, null, null));
+// tree.insert(new Node(9, 0, null, null));
+// tree.insert(new Node(13, 0, null, null));
+// tree.insert(new Node(16, 0, null, null));
+
+for(let i=1;i<20;i++){
+    let val=random(30);
+    tree.insert(new Node(val, 0, null, null));
+}
 
 let node13 = tree.find(8);
+tree.print();
+tree.delete(8);
 tree.print();
 tree.delete(8);
 tree.print();
