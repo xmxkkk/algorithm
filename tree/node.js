4eb3fa2495015d56ffe5ac9a9f4414245afe999f
xu.mingxun <xu.mingxun@puscene.com>
2018-10-26 17:03:38 +0800

    add

--- tree/node.js
+++
@@ -1,9 +0,0 @@
-let Node=function(data,color,leftChild,rightChild){
-    this.data=data;
-    this.color=color;
-    this.leftChild=leftChild;
-    this.rightChild=rightChild;
-}
-
-
-module.exports=Node;
