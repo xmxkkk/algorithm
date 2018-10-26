668f6c8a0c3ac9f4bce24083147cdf14b90971ef
xu.mingxun <xu.mingxun@puscene.com>
2018-10-26 14:57:52 +0800

    add

--- sort/sort_test.js
+++ sort/sort_test.js
@@ -1,6 +1,6 @@
-// var sort=require("./quick_sort_my.js");
+var sort=require("./quick_sort_my.js");
 // var sort = require("./quick_sort.js");
-var sort = require("./merge_sort_my.js");
+// var sort = require("./merge_sort_my.js");
 // var sort = require("./merge_sort.js");
 // var sort=require('./insert_sort.js');
 // var sort = require('./insert_sort_my.js');
